059c5ccd67c77ee9581b5d27ebc682c94af95b39
Md. Ibrahim Hafeez <mdibrahimhaf6996@gmail.com>
2023-07-28 18:06:04 +0530

    Modified BaseuRL

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,6 @@
-const baseUrl = "http://localhost:4000";
+const MODE = import.meta.env.MODE;
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
+const baseUrl = MODE === "development" ? "http://localhost:4000" : SERVER_URL;
 
 export default baseUrl;
